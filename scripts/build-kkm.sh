fe1636885ed9478ef6c0efb425a85f65225ae4c5
Serge Pashenkov <serge@kontain.app>
2022-11-15 12:47:02 -0700

    shim location

--- scripts/build-kkm.sh
+++ scripts/build-kkm.sh
@@ -2,16 +2,15 @@
 
 readonly storage="/tmp"
 
-INSTALL_DIR="/opt/kontain/bin"
+INSTALL_DIR="/opt/kontain"
+sudo mkdir -p ${INSTALL_DIR}
 
-sudo tar -xf ${storage}/kontain_bin.tar.gz  -C ${storage}
+sudo tar -xf ${storage}/kontain_bin.tar.gz -C ${INSTALL_DIR}
 
 # Compile and install kkm driver
 echo "build and install KKM driver"
-sudo ${storage}/kkm.run
+sudo chmod +x ${INSTALL_DIR}/bin/kkm.run
+sudo ${INSTALL_DIR}/bin/kkm.run
 
 # Install KM Binaries
-sudo mkdir -p ${INSTALL_DIR}
-sudo cp ${storage}/km/km ${INSTALL_DIR}/km
-sudo cp ${storage}/container-runtime/krun-label-trigger ${INSTALL_DIR}/krun-label-trigger
-sudo cp ${storage}/cloud/k8s/deploy/shim/containerd-shim-krun-v2 /usr/bin/containerd-shim-krun-v2
+sudo cp ${INSTALL_DIR}/shim/containerd-shim-krun-v2 /usr/bin/containerd-shim-krun-v2
